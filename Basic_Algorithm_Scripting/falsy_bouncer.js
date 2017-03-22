3e8638d4518c27a7228337b134c0410aa1e55c22
Regina Galambos <gina.galambos@gmail.com>
2017-03-22 02:01:21 +0100

    Challenge complete

--- Basic_Algorithm_Scripting/falsy_bouncer.js
+++ Basic_Algorithm_Scripting/falsy_bouncer.js
@@ -12,7 +12,7 @@ function bouncer(arr) {
 
 bouncer([7, "ate", "", false, 9]);
 
-console.log(bouncer([7, "ate", "", false, 9]))
-console.log(bouncer(["a", "b", "c"]))
-console.log(bouncer([false, null, 0, NaN, undefined, ""]))
-console.log(bouncer([1, null, NaN, 2, undefined]))
+console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer(["a", "b", "c"]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([1, null, NaN, 2, undefined]));
