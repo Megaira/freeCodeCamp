317cf894576d09756d76e705af9c6c890eed85ee
Regina Galambos <gina.galambos@gmail.com>
2017-03-29 02:29:55 +0200

    JS for current weather data display done

--- Weather_App/js/weather.js
+++ Weather_App/js/weather.js
@@ -2,25 +2,53 @@
   var lat;
   var lon;
   var weatherURL;
+  var unit = 'metric';
 
-  function showPosition(position) {
+  function showWeather(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    $('#location').html('lat: ' + position.coords.latitude + '<br>lon: ' + position.coords.longitude);
+    // lat = 51.509865;
+    // lon = -0.118092;
     console.log(lat, lon);
-    weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&APPID=ae44e8d67a569c6b8064d639567e8cec';
+    weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&units=' + unit + '&APPID=ae44e8d67a569c6b8064d639567e8cec';
 
     $.getJSON(weatherURL, function(result) {
-      var city = result.city.name;
-      $('#location').append('<br>' + city);
-      $("#weather").html(JSON.stringify(result));
+      var city = result.name;
+      var tempCelsius = Math.floor(result.main.temp);
+      var weatherId = result.weather[0].id;
+      var weatherStatus = result.weather[0].description;
+
+      var prefix = 'wi wi-owm-';
+      var code = weatherId;
+      var icon = '';
+
+      // if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+      //   // icon = 'day-' + icon;
+      // }
+      icon = prefix + code;
+
+      $('.location p').text(city);
+      $('.weather-icon i').attr('class', icon);
+      $('.weather-status p').text(weatherStatus);
+      $('.temperature').html('<span class="degrees">' + tempCelsius + '&#176;C</span>');
+
+      // console.log(city);
+      // console.log(icon);
+      // console.log(tempCelsius);
+      // console.log(tempFahrenheit);
+      // console.log(tempHigh);
+      // console.log(tempLow);
+
       console.log(result);
+
+      // console.log(weatherId);
+      // console.log(weatherStatus);
     });
   }
 
 
 $(document).ready(function(){
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showWeather);
   }
 });
