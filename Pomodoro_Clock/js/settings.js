fbf90de93e482fcdaa7fc65004a7730ab023c79d
Regina Galambos <gina.galambos@gmail.com>
2017-04-06 04:37:48 +0200

    Property values update now with button clicks

--- Pomodoro_Clock/js/settings.js
+++ Pomodoro_Clock/js/settings.js
@@ -16,20 +16,14 @@ var intervalNumInput = $('.num-sessions');
 // Canvas (needs function to write on canvas - later):
 var clockTimer = $('.timer');
 
-// Set Timer:
-// Write initial values in containers #timer, sessionInput, shortBreakInput
-function setTimer(container, timerSetting) {
-  container.text(timerSetting);
-  console.log(container.selector + ':' + timerSetting);
-  return timerSetting;
-}
-
 // Take user input and update timer lengths in Settings:
-function customSettings(button1, button2, timerSetting, container) {
+function customSettings(button1, button2, key, timerSetting, container) {
   button1.on('click', function() {
     timerSetting += 1;
     container.text(timerSetting);
-    setTimer(container, timerSetting);
+    Clock[key] = timerSetting;
+    Clock.setClock(timerSetting);
+
   })
   button2.on('click', function() {
     timerSetting -= 1;
@@ -37,13 +31,12 @@ function customSettings(button1, button2, timerSetting, container) {
       timerSetting = 0;
     }
     container.text(timerSetting);
-    setTimer(container, timerSetting);
+    Clock[key] = timerSetting;
+    Clock.setClock(timerSetting);
   });
 }
 // Call Setting function with variables:
-// customSettings(sessionPlus, sessionMinus, sessionLength, clockTimer);
-customSettings(sessionPlus, sessionMinus, workInterval, sessionInput);
-customSettings(shortBreakPlus, shortBreakMinus, shortBreak, shortBreakInput);
-customSettings(longBreakPlus, longBreakMinus, longBreak, longBreakInput);
-customSettings(intervalNumPlus, intervalNumMinus, intervalNum, intervalNumInput);
-console.log(workInterval, shortBreak, longBreak, intervalNum);
+customSettings(sessionPlus, sessionMinus, 'workInterval', Clock.workInterval, sessionInput);
+customSettings(shortBreakPlus, shortBreakMinus, 'shortBreak', Clock.shortBreak, shortBreakInput);
+customSettings(longBreakPlus, longBreakMinus, 'longBreak', Clock.longBreak, longBreakInput);
+customSettings(intervalNumPlus, intervalNumMinus, 'intervalNum', Clock.intervalNum, intervalNumInput);
