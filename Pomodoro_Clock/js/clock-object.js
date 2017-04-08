153d33272866516ea9837da3fa7d0ef949f277b6
Regina Galambos <gina.galambos@gmail.com>
2017-04-09 00:58:54 +0200

    Added logic for status check and timer settings when timer hits 0

--- Pomodoro_Clock/js/clock-object.js
+++ Pomodoro_Clock/js/clock-object.js
@@ -2,20 +2,20 @@ var Clock = {
   workInterval: 25,
   shortBreak: 5,
   longBreak: 15,
-  intervalNum: 4,
+  intervalCount: 4,
   running: false,
-  intervalCount: 0,
+  // intervalCount: 0,
   status: 'ready', // 'pomodoro', 'coffee', 'book', 'pause'
   // Properties for updateClock:
   startTime: 0,
   duration: 0,
 
   setClock: function(timerSetting){
-    console.log(Clock.workInterval, Clock.shortBreak, Clock.longBreak, Clock.intervalNum);
+    console.log(Clock.workInterval, Clock.shortBreak, Clock.longBreak, Clock.intervalCount);
   },
   startClock: function() {
     Clock['running'] = true;
-    Clock['intervalCount'] += 1;
+    Clock['intervalCount'] -= 1;
     Clock['status'] = 'pomodoro';
     // Get start time for calculation
 
@@ -26,9 +26,9 @@ var Clock = {
     $('.stop').removeClass('hide').addClass('show');
 
     Clock.startTime = Date.now();
-    console.log(Clock.startTime);
+    // console.log(Clock.startTime);
     Clock.updateClock();
-    console.log(Clock);
+    // console.log(Clock);
 
   },
   pauseClock: function() {
@@ -66,24 +66,38 @@ var Clock = {
     console.log(Clock);
   },
   updateClock: function() {
-    // Conditionnals and cases:
+    Clock.duration = Clock.workInterval; // festlegen durch 'status'
+    // Clock.duration = Clock.duration * 60 * 1000;
+    // Conditionnals and status cases:
 
     // Calculate:
     var interval = setInterval(function () {
-      Clock.duration = 25;
-      Clock.duration = Clock.duration * 60 * 1000;
       var currentTime = Date.now();
-      var passedTime = currentTime - Clock.startTime;
-      var remainingTime = Clock.duration - passedTime;
+      var passedTime = currentTime - Clock.startTime; // dt
+      var remainingTime = Clock.duration * 60 * 1000;
+
+      // time left?
+      if (remainingTime > 0) {
+        // yes: reduce by dt
+        remainingTime -= passedTime;
+      } else { // no:
+        // Check status and intervalCount: switch(Clock.status)
+        // case 'pomodoro': (default? mit > 0)
+          // if intervalCount > 0: change status = coffee, duration = shortBreak, remainingTime -= passedTime
+          // if intervalCount == 0: change status = book, duration = longBreak, remainingTime -= passedTime
+        // case 'coffee': change status = pomodoro, duration = workInterval, intervalCount -=1, remainingTime -= passedTime
+        // case 'book': change status = pomodoro, duration = workInterval, intervalCount = Clock.intervalCount, remainingTime -= passedTime
+      }
 
       var minutes = Math.floor((remainingTime / 1000 / 60) % 60);
       var seconds = Math.round(remainingTime / 1000) % 60;
 
-      console.log(Clock.startTime);
-      console.log(currentTime);
-      console.log(passedTime);
-      console.log(remainingTime);
       console.log(minutes + ':' + seconds);
+      console.log(Clock.status);
+      console.log(Clock.intervalCount);
+
+      $('.minutes').text(minutes);
+      $('.seconds').text(seconds);
     }, 1000);
 
 
