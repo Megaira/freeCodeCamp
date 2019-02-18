413a4c4a538ab08c31e2ac48f9a54b03e0cd76aa
Regina Galambos <gina.galambos@gmail.com>
2019-02-18 02:46:27 +0100

    update fcc path structure

--- Algorithms_and_Data_Structures_Projects/palindrome_checker.js
+++ Algorithms_and_Data_Structures_Projects/palindrome_checker.js
@@ -1,22 +1,10 @@
 function palindrome(str) {
-  // Good luck!
-  console.log(str);
+  // Convert input string to lower case
+  // remove special characters
   var original = str.toLowerCase().replace(/\s/g, '').replace(/[^a-zA-Z0-9]/g, '');
+  // split string into characters
+  // reverse cleaned created array and join to string
   var reverseStr = original.split('').reverse().join('');
-  console.log(original);
-  console.log(reverseStr);
-  console.log(original === reverseStr);
+  // check if original string equals reversed string
+  return original === reverseStr;
 }
-
-// palindrome("eye");
-palindrome("_eye");
-// palindrome("race car");
-// palindrome("not a palindrome");
-palindrome("A man, a plan, a canal. Panama");
-// palindrome("never odd or even");
-// palindrome("nope");
-// palindrome("almostomla");
-palindrome("My age is 0, 0 si ega ym.");
-palindrome("1 eye for of 1 eye.");
-palindrome("0_0 (: /-\ :) 0-0");
-palindrome("five|\_/|four");
